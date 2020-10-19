97ff926a3061a960b309c5bb7208a2f631428d5a
mantou <64596631+kissmantou@users.noreply.github.com>
2020-10-19 21:53:07 +0800

    主页更新

--- js/hello.js
+++ js/hello.js
@@ -1,15 +1,18 @@
-alert(GetUrlPath())
+//alert(GetUrlPath()+"java/")
 
 function re(){
     window.location.href='../index.html';
 }
-
 
 function GetUrlPath(){
     var url = document.location.toString();				
     if(url.indexOf("/") != -1){
         url = url.substring(0,  url.lastIndexOf("/html/")) ;
 }
-
-    　　　　return url;
+    　　　　return url+'/file/';
 }
+
+
+
+
+/* -- 分割线-- */

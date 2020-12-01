b683b8fec58711022d54ddab28844e8056c1c6ec
mantou <64596631+kissmantou@users.noreply.github.com>
2020-12-02 01:00:54 +0800

    偷懒

--- js/content.js
+++ js/content.js
@@ -9,6 +9,8 @@ var 脚本 = new alo("阴阳师脚本 作者Vinight/Antony","一个自动挂机�
 
 var 迷宫 = new alo("迷宫 作者：Vinight/Antony","一个简单的迷宫，使用方向键移动，使用del键刷新地图，使用回车开始游戏",'配置pygame(可以使用pip一键安装);使用pyinstall打包');
 
+var 替换文件 = new alo("替换文件 作者：Vinight","简单的替换文件，做这个的原因是我懒得在消逝的光芒换mod来回复制粘贴文件，当然还带了点ui",'打开输入消逝的光芒路径就好了');
+
 
 
 //  -- java --
@@ -19,8 +21,10 @@ var 对比文本java = new alo("对比文本java版本 作者：Vinight","简单
 var 计算器 = new alo("计算器 作者：年纪小的Vinight","在2016年使用Android制作的计算器软件，由于水平非常业余，所以代码看起来非常谢特",'使用android studio(pc)或aide(android)打开直接编译运行');
 
 //  -- web --
-var 主站 = new alo("网站代码 作者: Vinight/Antony","与2020年11月2日大体正式完工，会逐步小更新",'解压开双击index.html直接运行，若需最新代码请按f12直接查看');
+var 主站 = new alo("网站代码 作者: Vinight","与2020年11月2日大体正式完工，会逐步小更新",'解压开双击index.html直接运行，若需最新代码请按f12直接查看');
 
 
 // -- c --
 var mario = new alo("名称及作者", "小简介", "使用方式")
+
+
--- js/hello.js
+++ js/hello.js
@@ -38,3 +38,4 @@ function playsound(){
     music_src.play();
 
 }
+

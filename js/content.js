89883120321b3bb43b92a499e25b3a25b8c08c24
mantou <64596631+kissmantou@users.noreply.github.com>
2020-12-10 19:26:58 +0800

    添加

--- js/content.js
+++ js/content.js
@@ -5,13 +5,13 @@ function alo(txt, info,con){
 } 
 
 //  -- python --
-var 脚本 = new alo("阴阳师脚本 作者Vinight/Antony","一个自动挂机刷阴阳师大多数副本的脚本",'配置adb;配置opencv(可以使用pip一键安装);使用pyinstall打包');
+var 脚本 = new alo("阴阳师脚本 作者Vinight/Antony","一个自动挂机刷阴阳师大多数副本的脚本,点开exe直接使用，不要广泛传播",'配置adb;配置opencv(可以使用pip一键安装);使用pyinstall打包');
 
 var 迷宫 = new alo("迷宫 作者：Vinight/Antony","一个简单的迷宫，使用方向键移动，使用del键刷新地图，使用回车开始游戏",'配置pygame(可以使用pip一键安装);使用pyinstall打包');
 
 var 替换文件 = new alo("替换文件 作者：Vinight","简单的替换文件，做这个的原因是我懒得在消逝的光芒换mod来回复制粘贴文件，当然还带了点ui",'打开输入消逝的光芒路径就好了');
 
-
+var 简单加密 = new alo("简单加密 作者：Vinight","用凯撒密码的方式加密文本，本身不难但是采取的是缩写形式",'使用方法为 jm(string,int,boolean)');
 
 //  -- java --
 var 对比文本java = new alo("对比文本java版本 作者：Vinight","简单的对比文本是否相符，可以返回错字位置和错字内容",'实例化TestString，参数1为string(原文本)，参数2为string(对比文本)');
@@ -26,5 +26,4 @@ var 主站 = new alo("网站代码 作者: Vinight","与2020年11月2日大体�
 
 // -- c --
 var mario = new alo("Antony直接弃坑", "没办法啊专门给c留下来一块地方，虽然弃坑了但是懒得删掉了", "等一段时间吧")
-
 
